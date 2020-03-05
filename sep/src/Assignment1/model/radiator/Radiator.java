1afeb7ab6f9e8ee6ef4729bd42ac25c3b8c13167
roxi2601 <roxi260111@gmail.com>
2020-03-05 09:20:42 +0100

    ooom

--- sep/src/Assignment1/model/radiator/Radiator.java
+++ sep/src/Assignment1/model/radiator/Radiator.java
@@ -1,26 +1,36 @@
 package Assignment1.model.radiator;
 
-public class Radiator  {
-private RadiatorState currentState;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 
-public Radiator()
-{
-  currentState = new OffState();
-}
-public void turnUp() throws InterruptedException
-{
-    currentState.turnUp(this);
-}
-public void turnDown()
-{
-    currentState.turnDown(this);
-}
-public int getPower()
-{
-   return currentState.getPower();
-}
-public void setPowerState(RadiatorState state)
-{
-    this.currentState=state;
-}
+public class Radiator {
+    private RadiatorState currentState;
+    private PropertyChangeSupport support;
+
+    public Radiator(PropertyChangeSupport support){
+        this.support = support;
+        currentState = new OffState();
+    }
+
+    public void turnUp() throws InterruptedException {
+        currentState.turnUp(this);
+    }
+
+    public void turnDown() {
+        currentState.turnDown(this);
+    }
+
+    public int getPower() {
+        return currentState.getPower();
+    }
+
+    public void setPowerState(RadiatorState state) {
+        RadiatorState oldState = currentState;
+        this.currentState = state;
+        support.firePropertyChange("state",oldState,currentState);
+    }
+    public void addListener(PropertyChangeListener listener)
+    {
+        support.addPropertyChangeListener(listener);
+    }
 }
--- sep/src/Assignment1/viewmodel/control/ControlViewModel.java
+++ sep/src/Assignment1/viewmodel/control/ControlViewModel.java
@@ -15,7 +15,7 @@ public class ControlViewModel {
         this.model = model;
         state = new SimpleStringProperty();
         //How to make view model see changed state from 3 to 2(caused by running thread in Power3State constructor)
-        //model.addListener("Radiator state", evt->getRadiatorPower());
+        model.addListener("Radiator state", evt->updateState());
     }
     public StringProperty stateProperty() {
         return state;
@@ -31,9 +31,15 @@ public class ControlViewModel {
       if(model.getRadiatorPower()!=0)
       model.turnDownRadiator();
     }
+    public void updateState()
+    {
+        int pwr = model.getRadiatorPower();
+        state.setValue(pwr+"");
+    }
+
     public int getRadiatorPower()
     {
         return model.getRadiatorPower();
-    }
 
+    }
 }
